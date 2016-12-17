a464af995855ddd553f733da2a335495eeaf9d2e
sdzbzjh <sdzbzjh@gmail.com>
2016-12-17 11:32:16 +0800

    完善选择笔迹粗细功能

--- js/draw.js
+++ js/draw.js
@@ -2,18 +2,25 @@
  * Created by zhengjh on 2016/12/16.
  */
 var penColor = "";
-var inputColor= document.getElementById('inputcolor');
+var penSize = 10;
+var inputColor = document.getElementById('inputcolor');
+var inputSize = document.getElementById('inputsize');
+//console.log(inputSize);
 document.getElementById('color').onclick = function () {
     inputColor.click();
 };
 inputColor.onchange = function () {
     penColor = this.value;
 };
+inputSize.onchange = function () {
+    //alert(this.value);
+    penSize = this.value;
+};
 var canvas_ = document.getElementsByTagName("canvas")[0];
 
 //全屏
-canvas_.setAttribute("width", screen.width);
-canvas_.setAttribute("height", screen.height);
+canvas_.setAttribute("width", window.screen.width);
+canvas_.setAttribute("height", window.screen.height);
 
 
 var context = canvas_.getContext("2d");
@@ -29,7 +36,7 @@ var m_down = false;
 
 function paint() {
     context.strokeStyle = penColor;
-    context.lineWidth = 10;
+    context.lineWidth = penSize;
     context.beginPath();
     context.moveTo(array_paint[0][0], array_paint[0][1]);
     if (array_paint.length == 1)
@@ -73,20 +80,20 @@ canvas_.onmousemove = function (event) {
         paint();
     }
 };
-var pen=document.getElementById('pen');
-var eraser=document.getElementById('eraser');
-var clearCanvas=document.getElementById('clearCanvas');
-eraser.onclick=function () {
-    penColor='#ffffff';
-    eraser.src='image/eraser_click.jpg';
-    pen.src='image/pen.jpg';
+var pen = document.getElementById('pen');
+var eraser = document.getElementById('eraser');
+var clearCanvas = document.getElementById('clearCanvas');
+eraser.onclick = function () {
+    penColor = '#ffffff';
+    eraser.src = 'image/eraser_click.jpg';
+    pen.src = 'image/pen.jpg';
 };
-pen.onclick=function () {
-    penColor=inputColor.value;
-    pen.src='image/pen_click.jpg';
-    eraser.src='image/eraser.jpg';
+pen.onclick = function () {
+    penColor = inputColor.value;
+    pen.src = 'image/pen_click.jpg';
+    eraser.src = 'image/eraser.jpg';
 };
 //清除画布内容
-clearCanvas.onclick=function () {
-    context.clearRect(0,0,canvas_.width,canvas_.height);
+clearCanvas.onclick = function () {
+    context.clearRect(0, 0, canvas_.width, canvas_.height);
 };
